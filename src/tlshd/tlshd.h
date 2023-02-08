e7102985dec06e29bb1ec307053cad779b86902c
Chuck Lever <chuck.lever@oracle.com>
2023-02-08 13:24:59 -0500

    tlshd: Add struct tlshd_handshake_parms
    Refactor: Group pertinent handshake parameters into one struct so
    they can be filled in by a single operation.
    
    More parameters will be added to this struct in subsequent patches.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- src/tlshd/tlshd.h
+++ src/tlshd/tlshd.h
@@ -23,8 +23,13 @@ extern int tlshd_library_debug;
 extern int tlshd_stderr;
 extern GKeyFile *tlshd_configuration;
 
+struct tlshd_handshake_parms {
+	char		*peername;
+	int		sockfd;
+};
+
 /* client.c */
-extern void tlshd_clienthello_handshake(int sock, const char *peername);
+extern void tlshd_clienthello_handshake(struct tlshd_handshake_parms *parms);
 
 /* config.c */
 bool tlshd_config_init(const gchar *pathname);
