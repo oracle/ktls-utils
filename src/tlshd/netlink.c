bc1ad8d7f236b87f413e6db5d91b40b6c2ba2455
Chuck Lever <chuck.lever@oracle.com>
2023-02-08 14:21:27 -0500

    tlshd: Introduce infrastructure for using NETLINK_HANDSHAKE
    The Linux kernel now supports a NETLINK facility to listen for and
    manage kernel consumer requests for TLS handshakes. This facility
    replaces the AF_TLSH listener and using socket options for
    retrieving handshake parameters.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

---
+++ src/tlshd/netlink.c
@@ -0,0 +1,414 @@
+/*
+ * Netlink operations for tlshd
+ *
+ * Copyright (c) 2023 Oracle and/or its affiliates.
+ *
+ * ktls-utils is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <keyutils.h>
+
+#include <gnutls/gnutls.h>
+#include <gnutls/abstract.h>
+#include <gnutls/x509.h>
+#include <linux/tls.h>
+
+#include <netlink/netlink.h>
+#include <netlink/msg.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+
+#include <glib.h>
+
+#include "tlshd.h"
+#include "netlink.h"
+
+/**
+ * tlshd_genl_sock_open - get a connected netlink socket
+ * @sock: OUT: on success, set to a connected netlink socket
+ *
+ * Returns zero on success or a negative errno.
+ */
+int tlshd_genl_sock_open(struct nl_sock **sock)
+{
+	struct nl_sock *nls;
+	int err, ret;
+
+	ret = -ENOMEM;
+	nls = nl_socket_alloc();
+	if (!nls) {
+		tlshd_log_error("Failed to allocate netlink socket.");
+		goto out;
+	}
+
+	ret = -ENOLINK;
+	err = genl_connect(nls);
+	if (err != NLE_SUCCESS) {
+		tlshd_log_nl_error("genl_connect", err);
+		nl_socket_free(nls);
+		goto out;
+	}
+
+	*sock = nls;
+	ret = 0;
+out:
+	return ret;
+}
+
+/**
+ * tlshd_genl_mcgrp_join - subscribe a listener socket to a multicast group
+ * @nls: socket to subscribe
+ *
+ * Returns zero on success or a negative errno.
+ */
+int tlshd_genl_mcgrp_join(struct nl_sock *nls)
+{
+	int err, ret, mcgrp;
+
+	ret = -ENOLINK;
+	mcgrp = genl_ctrl_resolve_grp(nls, HANDSHAKE_GENL_NAME,
+				      HANDSHAKE_GENL_MCGRP_TLS_NAME);
+	if (mcgrp < 0) {
+		tlshd_log_nl_error("genl_ctrl_resolve_grp", mcgrp);
+		goto out;
+	}
+
+	ret = -ENOENT;
+	err = nl_socket_add_membership(nls, mcgrp);
+	if (err != NLE_SUCCESS) {
+		tlshd_log_nl_error("nl_socket_add_membership", err);
+		goto out;
+	}
+
+	nl_socket_disable_seq_check(nls);
+	ret = 0;
+
+out:
+	return ret;
+}
+
+/**
+ * tlshd_genl_sock_close - release a netlink socket
+ * @sock: socket to release
+ *
+ */
+void tlshd_genl_sock_close(struct nl_sock *nls)
+{
+	if (!nls)
+		return;
+
+	nl_close(nls);
+	nl_socket_free(nls);
+}
+
+static const struct nla_policy
+tlshd_genl_policy[HANDSHAKE_GENL_ATTR_MAX + 1] = {
+	[HANDSHAKE_GENL_ATTR_MSG_STATUS]	= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_SESS_STATUS]	= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_SOCKFD]		= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_PROTOCOL]		= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_ACCEPT]		= { .type = NLA_NESTED, },
+	[HANDSHAKE_GENL_ATTR_DONE]		= { .type = NLA_NESTED, },
+};
+
+static const struct nla_policy
+tlshd_genl_tls_accept_policy[HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX + 1] = {
+	[HANDSHAKE_GENL_ATTR_TLS_TYPE]		= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_TLS_AUTH]		= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_TLS_PRIORITIES]	= { .type = NLA_NUL_STRING, },
+	[HANDSHAKE_GENL_ATTR_TLS_X509_CERT]	= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_TLS_X509_PRIVKEY]	= { .type = NLA_U32, },
+	[HANDSHAKE_GENL_ATTR_TLS_PSK]		= { .type = NLA_U32, },
+};
+
+static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
+{
+	struct nlattr *tb[HANDSHAKE_GENL_ATTR_MAX + 1];
+	struct nlattr *resp[HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX + 1];
+	struct tlshd_handshake_parms *parms = arg;
+	int err;
+
+	err = genlmsg_parse(nlmsg_hdr(msg), 0, tb, HANDSHAKE_GENL_ATTR_MAX,
+			    tlshd_genl_policy);
+	if (err < 0) {
+		tlshd_log_nl_error("genlmsg_parse", err);
+		return NL_STOP;
+	}
+
+	if (tb[HANDSHAKE_GENL_ATTR_MSG_STATUS]) {
+		parms->msg_status = nla_get_u32(tb[HANDSHAKE_GENL_ATTR_MSG_STATUS]);
+		return NL_STOP;
+	}
+	parms->msg_status = 0;
+
+	if (tb[HANDSHAKE_GENL_ATTR_SOCKFD])
+		parms->sockfd = nla_get_u32(tb[HANDSHAKE_GENL_ATTR_SOCKFD]);
+
+	/* Nested TLS-specific attributes */
+
+	err = nla_parse_nested(resp, HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX,
+			       tb[HANDSHAKE_GENL_ATTR_ACCEPT],
+			       tlshd_genl_tls_accept_policy);
+	if (err < 0) {
+		tlshd_log_nl_error("nla_parse_nested", err);
+		return NL_STOP;
+	}
+
+	parms->handshake_type = HANDSHAKE_GENL_TLS_TYPE_UNSPEC;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_TYPE])
+		parms->handshake_type = nla_get_u32(resp[HANDSHAKE_GENL_ATTR_TLS_TYPE]);
+	parms->auth_type = HANDSHAKE_GENL_TLS_AUTH_UNSPEC;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_AUTH])
+		parms->auth_type = nla_get_u32(resp[HANDSHAKE_GENL_ATTR_TLS_AUTH]);
+	parms->priorities = TLS_DEFAULT_PRIORITIES;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_PRIORITIES])
+		parms->priorities =
+			strdup(nla_get_string(resp[HANDSHAKE_GENL_ATTR_TLS_PRIORITIES]));
+	parms->x509_cert = TLS_NO_CERT;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_X509_CERT])
+		parms->x509_cert = nla_get_u32(resp[HANDSHAKE_GENL_ATTR_TLS_X509_CERT]);
+	parms->x509_privkey = TLS_NO_PRIVKEY;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_X509_PRIVKEY])
+		parms->x509_privkey =
+			nla_get_u32(resp[HANDSHAKE_GENL_ATTR_TLS_X509_PRIVKEY]);
+	parms->peerid = TLS_NO_PEERID;
+	if (resp[HANDSHAKE_GENL_ATTR_TLS_PSK])
+		parms->peerid = nla_get_u32(resp[HANDSHAKE_GENL_ATTR_TLS_PSK]);
+
+	return NL_SKIP;
+}
+
+/**
+ * tlshd_genl_get_handshake_parms - Retrieve handshake parameters
+ * @parms: buffer to fill in with parameters
+ *
+ * Returns 0 if handshake parameters were retrieved successfully.
+ * Caller must free the string returned in parms->priorities with
+ * free(3).
+ *
+ * Otherwise a negative errno is returned, and the content of
+ * @parms is indeterminant.
+ */
+int tlshd_genl_get_handshake_parms(struct tlshd_handshake_parms *parms)
+{
+	int family_id, err, ret;
+	struct nlmsghdr *hdr;
+	struct nl_sock *nls;
+	struct nl_msg *msg;
+	struct nl_cb *cb;
+
+	tlshd_log_debug("Querying the handshake service\n");
+	parms->msg_status = -EIO;
+
+	ret = tlshd_genl_sock_open(&nls);
+	if (ret)
+		return ret;
+
+	ret = -ESRCH;
+	family_id = genl_ctrl_resolve(nls, HANDSHAKE_GENL_NAME);
+	if (family_id < 0) {
+		tlshd_log_error("Failed to resolve netlink service.");
+		goto out_close;
+	}
+
+	ret = -ENOMEM;
+	cb = nl_cb_alloc(NL_CB_DEFAULT);
+	if (!cb) {
+		tlshd_log_error("Failed to allocate netlink callback.");
+		goto out_close;
+	}
+	nl_cb_set(cb, NL_CB_VALID, NL_CB_CUSTOM, tlshd_genl_valid_handler, parms);
+
+	msg = nlmsg_alloc();
+	if (!msg) {
+		tlshd_log_error("Failed to allocate message buffer.");
+		goto out_cb_put;
+	}
+
+	ret = -EIO;
+	hdr = genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family_id,
+			  0, 0, HANDSHAKE_GENL_CMD_ACCEPT, 0);
+	if (!hdr) {
+		tlshd_log_error("Failed to set up message header.");
+		goto out_msgfree;
+	}
+
+	err = nla_put_u32(msg, HANDSHAKE_GENL_ATTR_PROTOCOL,
+			  HANDSHAKE_GENL_PROTO_TLS);
+	if (err < 0) {
+		tlshd_log_nl_error("nla_put protocol", err);
+		goto out_msgfree;
+	}
+
+	nl_socket_disable_auto_ack(nls);
+	err = nl_send_auto(nls, msg);
+	if (err < 0) {
+		tlshd_log_nl_error("nl_send_auto", err);
+		goto out_msgfree;
+	}
+
+	ret = -EINVAL;
+	err = nl_recvmsgs(nls, cb);
+	if (err < 0) {
+		tlshd_log_nl_error("nl_recvmsgs", err);
+		goto out_msgfree;
+	}
+
+	ret = 0;
+
+out_msgfree:
+	nlmsg_free(msg);
+out_cb_put:
+	nl_cb_put(cb);
+out_close:
+	tlshd_genl_sock_close(nls);
+	return ret;
+}
+
+/**
+ * tlshd_genl_done_status - Indicate handshake has failed
+ * @parms: buffer filled in with parameters
+ *
+ */
+void tlshd_genl_done_status(struct tlshd_handshake_parms *parms)
+{
+	struct nlmsghdr *hdr;
+	struct nl_sock *nls;
+	struct nl_msg *msg;
+	int family_id, err;
+
+	err = tlshd_genl_sock_open(&nls);
+	if (err)
+		return;
+
+	family_id = genl_ctrl_resolve(nls, HANDSHAKE_GENL_NAME);
+	if (family_id < 0) {
+		tlshd_log_nl_error("genl_ctrl_resolve", err);
+		goto out_close;
+	}
+
+	msg = nlmsg_alloc();
+	if (!msg) {
+		tlshd_log_error("Failed to allocate message buffer.");
+		goto out_close;
+	}
+
+	hdr = genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family_id,
+			  0, 0, HANDSHAKE_GENL_CMD_DONE, 0);
+	if (!hdr) {
+		tlshd_log_error("Failed to set up message header.");
+		goto out_free;
+	}
+
+	err = nla_put_u32(msg, HANDSHAKE_GENL_ATTR_SOCKFD, parms->sockfd);
+	if (err < 0) {
+		tlshd_log_nl_error("nla_put sockfd", err);
+		goto out_free;
+	}
+
+	err = nla_put_u32(msg, HANDSHAKE_GENL_ATTR_SESS_STATUS,
+			  parms->session_status);
+	if (err) {
+		tlshd_log_nl_error("nla_put sess_status", err);
+		goto out_free;
+	}
+
+	nl_socket_disable_auto_ack(nls);
+	err = nl_send_auto(nls, msg);
+	if (err < 0) {
+		tlshd_log_nl_error("nl_send_auto", err);
+		goto out_free;
+	}
+
+out_free:
+	nlmsg_free(msg);
+out_close:
+	tlshd_genl_sock_close(nls);
+}
+
+/**
+ * tlshd_genl_done - Indicate anon handshake has completed successfully
+ * @parms: buffer filled in with parameters
+ *
+ */
+void tlshd_genl_done(struct tlshd_handshake_parms *parms)
+{
+	struct nlmsghdr *hdr;
+	struct nl_sock *nls;
+	struct nl_msg *msg;
+	int family_id, err;
+
+	err = tlshd_genl_sock_open(&nls);
+	if (err)
+		return;
+
+	family_id = genl_ctrl_resolve(nls, HANDSHAKE_GENL_NAME);
+	if (family_id < 0) {
+		tlshd_log_nl_error("genl_ctrl_resolve", err);
+		goto out_close;
+	}
+
+	msg = nlmsg_alloc();
+	if (!msg) {
+		tlshd_log_error("Failed to allocate message buffer.");
+		goto out_close;
+	}
+
+	hdr = genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family_id,
+			  0, 0, HANDSHAKE_GENL_CMD_DONE, 0);
+	if (!hdr) {
+		tlshd_log_error("Failed to set up message header.");
+		goto out_free;
+	}
+
+	err = nla_put_u32(msg, HANDSHAKE_GENL_ATTR_SOCKFD, parms->sockfd);
+	if (err < 0) {
+		tlshd_log_nl_error("nla_put sockfd", err);
+		goto out_free;
+	}
+
+	err = nla_put_u32(msg, HANDSHAKE_GENL_ATTR_SESS_STATUS,
+			  parms->session_status);
+	if (err) {
+		tlshd_log_nl_error("nla_put sess_status", err);
+		goto out_free;
+	}
+
+	nl_socket_disable_auto_ack(nls);
+	err = nl_send_auto(nls, msg);
+	if (err < 0) {
+		tlshd_log_nl_error("nl_send_auto", err);
+		goto out_free;
+	}
+
+out_free:
+	nlmsg_free(msg);
+out_close:
+	tlshd_genl_sock_close(nls);
+}
---
+++ src/tlshd/netlink.h
@@ -0,0 +1,113 @@
+/*
+ * GENL HANDSHAKE service.
+ *
+ * Author: Chuck Lever <chuck.lever@oracle.com>
+ *
+ * Copyright (c) 2023, Oracle and/or its affiliates.
+ *
+ * ktls-utils is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/*
+ * Data structures and functions that are visible to user space are
+ * declared here. This file constitutes an API contract between the
+ * Linux kernel and user space.
+ */
+
+#ifndef _UAPI_LINUX_HANDSHAKE_H
+#define _UAPI_LINUX_HANDSHAKE_H
+
+#define HANDSHAKE_GENL_NAME	"handshake"
+#define HANDSHAKE_GENL_VERSION	0x01
+
+enum handshake_genl_mcgrps {
+	HANDSHAKE_GENL_MCGRP_NONE = 0,
+	HANDSHAKE_GENL_MCGRP_TLS,
+};
+
+#define HANDSHAKE_GENL_MCGRP_NONE_NAME	"none"
+#define HANDSHAKE_GENL_MCGRP_TLS_NAME	"tls"
+
+enum handshake_genl_cmds {
+	HANDSHAKE_GENL_CMD_UNSPEC = 0,
+	HANDSHAKE_GENL_CMD_READY,
+	HANDSHAKE_GENL_CMD_ACCEPT,
+	HANDSHAKE_GENL_CMD_DONE,
+
+	__HANDSHAKE_GENL_CMD_MAX
+};
+#define HANDSHAKE_GENL_CMD_MAX	(__HANDSHAKE_GENL_CMD_MAX - 1)
+
+enum handshake_genl_attrs {
+	HANDSHAKE_GENL_ATTR_UNSPEC = 0,
+	HANDSHAKE_GENL_ATTR_MSG_STATUS,
+	HANDSHAKE_GENL_ATTR_SESS_STATUS,
+	HANDSHAKE_GENL_ATTR_SOCKFD,
+	HANDSHAKE_GENL_ATTR_PROTOCOL,
+
+	HANDSHAKE_GENL_ATTR_ACCEPT,
+	HANDSHAKE_GENL_ATTR_DONE,
+
+	__HANDSHAKE_GENL_ATTR_MAX
+};
+#define HANDSHAKE_GENL_ATTR_MAX	(__HANDSHAKE_GENL_ATTR_MAX - 1)
+
+
+/*
+ * Items specific to TLSv1.3 handshakes
+ */
+
+enum handshake_tls_accept_attrs {
+	HANDSHAKE_GENL_ATTR_TLS_ACCEPT_UNSPEC = 0,
+	HANDSHAKE_GENL_ATTR_TLS_TYPE,
+	HANDSHAKE_GENL_ATTR_TLS_AUTH,
+	HANDSHAKE_GENL_ATTR_TLS_PRIORITIES,
+	HANDSHAKE_GENL_ATTR_TLS_X509_CERT,
+	HANDSHAKE_GENL_ATTR_TLS_X509_PRIVKEY,
+	HANDSHAKE_GENL_ATTR_TLS_PSK,
+
+	__HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX
+};
+#define HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX \
+	(__HANDSHAKE_GENL_ATTR_TLS_ACCEPT_MAX - 1)
+
+enum handshake_tls_done_attrs {
+	HANDSHAKE_GENL_ATTR_TLS_DONE_UNSPEC = 0,
+	HANDSHAKE_GENL_ATTR_TLS_REMOTE_PEERID,
+
+	__HANDSHAKE_GENL_ATTR_TLS_DONE_MAX
+};
+#define HANDSHAKE_GENL_ATTR_TLS_DONE_MAX \
+	(__HANDSHAKE_GENL_ATTR_TLS_DONE_MAX - 1)
+
+enum handshake_genl_protocol {
+	HANDSHAKE_GENL_PROTO_UNSPEC = 0,
+	HANDSHAKE_GENL_PROTO_TLS,
+};
+
+enum handshake_genl_tls_type {
+	HANDSHAKE_GENL_TLS_TYPE_UNSPEC = 0,
+	HANDSHAKE_GENL_TLS_TYPE_CLIENTHELLO,
+	HANDSHAKE_GENL_TLS_TYPE_SERVERHELLO,
+};
+
+enum handshake_genl_tls_auth {
+	HANDSHAKE_GENL_TLS_AUTH_UNSPEC = 0,
+	HANDSHAKE_GENL_TLS_AUTH_UNAUTH,
+	HANDSHAKE_GENL_TLS_AUTH_X509,
+	HANDSHAKE_GENL_TLS_AUTH_PSK,
+};
+
+#endif /* _UAPI_LINUX_HANDSHAKE_H */
