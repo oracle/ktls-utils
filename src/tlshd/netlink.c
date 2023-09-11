oracle/ktls-utils

commit b586f7d97795b6c9f3b0aae17a1b1a82bbd5933b

tlshd: Reverse DNS lookup of peername
If the peername happens to be an IP address, it needs to be
converted to a domain name before using it for Server Name
Identification.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/netlink.c
@@ -257,6 +257,21 @@ static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 	tlshd_parse_peer_identity(parms, tb[HANDSHAKE_A_ACCEPT_PEER_IDENTITY]);
 	tlshd_parse_certificate(parms, tb[HANDSHAKE_A_ACCEPT_CERTIFICATE]);
 
+	/* SNI needs a domain name. If the kernel provided an IP
+	 * address, just use the reverse lookup results below. */
+	if (peername) {
+		static const struct addrinfo hints = {
+			.ai_family	= AF_UNSPEC,
+			.ai_flags	= AI_NUMERICHOST,
+		};
+		struct addrinfo *res;
+
+		err = getaddrinfo(peername, NULL, &hints, &res);
+		if (!err) {
+			freeaddrinfo(res);
+			peername = NULL;
+		}
+	}
 	if (peername)
 		strcpy(tlshd_peername, peername);
 	else {

commit d7d364c75ab68237097907f39898b28cb02c5bd9

tlshd: Revert b586f7d97795
Ben Hutchings observed that when tlshd handles the client side of a
handshake, it may get either an IP address or domain name from the
kernel, depending on how the target server was specified in the
mount attempt. The result of a reverse DNS lookup is used for
verifying the server's certificate, but that result is not
necessarily a secure piece of information.

The reason this reverse query was added was to enable SNI when
mounting NFS servers by IP address. Mounting by IP address is
needed, in particular, for pNFS and fs_locations results that
contain only IP addresses.

Reverting this commit temporarily breaks mounting by IP address with
TLS. However, recreating the server's certificate with a
subjectAltName field that lists the NFS server's IP address is
enough to enable NFS clients that mount by IP to securely verify the
server without using SNI.

Addresses Issue #22.

Fixes: b586f7d97795 ("tlshd: Reverse DNS lookup of peername")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/netlink.c
@@ -268,21 +268,6 @@ static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 	tlshd_parse_peer_identity(parms, tb[HANDSHAKE_A_ACCEPT_PEER_IDENTITY]);
 	tlshd_parse_certificate(parms, tb[HANDSHAKE_A_ACCEPT_CERTIFICATE]);
 
-	/* SNI needs a domain name. If the kernel provided an IP
-	 * address, just use the reverse lookup results below. */
-	if (peername) {
-		static const struct addrinfo hints = {
-			.ai_family	= AF_UNSPEC,
-			.ai_flags	= AI_NUMERICHOST,
-		};
-		struct addrinfo *res;
-
-		err = getaddrinfo(peername, NULL, &hints, &res);
-		if (!err) {
-			freeaddrinfo(res);
-			peername = NULL;
-		}
-	}
 	if (peername)
 		strcpy(tlshd_peername, peername);
 	else {