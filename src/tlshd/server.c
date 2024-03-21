b9901415581b6c177ba55f6f51717ec8468e68db
Hannes Reinecke <hare@suse.de>
2024-03-21 12:34:55 +0100

    server: add missing priority setting
    Commit 591022d ("ktls: move priority setting out of start_ktls_handshake()")
    only modified the client side and forgot to modify the server side.
    
    Fixes: #49
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- src/tlshd/server.c
+++ src/tlshd/server.c
@@ -258,6 +258,12 @@ static void tlshd_server_x509_handshake(struct tlshd_handshake_parms *parms)
 					       tlshd_server_x509_verify_function);
 	gnutls_certificate_server_set_request(session, GNUTLS_CERT_REQUEST);
 
+	ret = tlshd_gnutls_priority_set(session, parms);
+	if (ret) {
+		tlshd_log_gnutls_error(ret);
+		goto out_free_creds;
+	}
+
 	tlshd_start_tls_handshake(session, parms);
 
 	gnutls_deinit(session);
@@ -330,6 +336,12 @@ static void tlshd_server_psk_handshake(struct tlshd_handshake_parms *parms)
 	gnutls_session_set_ptr(session, parms);
 
 	gnutls_credentials_set(session, GNUTLS_CRD_PSK, psk_cred);
+
+	ret = tlshd_gnutls_priority_set(session, parms);
+	if (ret) {
+		tlshd_log_gnutls_error(ret);
+		goto out_free_creds;
+	}
 
 	tlshd_start_tls_handshake(session, parms);
 
