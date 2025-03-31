oracle/ktls-utils

commit 63287b9f826403fff3b5fc8f3bab6edcd84031d3

tlshd: Remove implicit sign conversion
Change of signedness on implicit conversion

Reported-by: Parfait 10.2 (#2101)
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/config.c
@@ -59,6 +59,7 @@ bool tlshd_config_init(const gchar *pathname)
 	gchar **keyrings;
 	gsize i, length;
 	GError *error;
+	gint tmp;
 
 	tlshd_configuration = g_key_file_new();
 
@@ -81,8 +82,9 @@ bool tlshd_config_init(const gchar *pathname)
 						 "debug", "tls", NULL);
 	nl_debug = g_key_file_get_integer(tlshd_configuration, "debug",
 					  "nl", NULL);
-	tlshd_delay_done = g_key_file_get_integer(tlshd_configuration, "debug",
-						  "delay_done", NULL);
+	tmp = g_key_file_get_integer(tlshd_configuration, "debug",
+				     "delay_done", NULL);
+	tlshd_delay_done = tmp > 0 ? tmp : 0;
 
 	keyrings = g_key_file_get_string_list(tlshd_configuration,
 					      "authentication",

src/tlshd/netlink.c
@@ -50,7 +50,7 @@
 #include "tlshd.h"
 #include "netlink.h"
 
-int tlshd_delay_done;
+unsigned int tlshd_delay_done;
 
 static int tlshd_genl_sock_open(struct nl_sock **sock)
 {

src/tlshd/tlshd.h
@@ -22,7 +22,7 @@
 
 extern int tlshd_debug;
 extern int tlshd_tls_debug;
-extern int tlshd_delay_done;
+extern unsigned int tlshd_delay_done;
 extern int tlshd_stderr;
 
 struct nl_sock;

commit 84e9454ddc22ce7e565a86fa35b59dec14e501ae

tlshd: Change of signedness on implicit conversion
Add an implicit type cast to show that negative values of "tmp"
are safely handled.

Reported-by: Parfait 9.0 (#2121)
Fixes: 63287b9f8264 ("tlshd: Remove implicit sign conversion")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/config.c
@@ -84,7 +84,7 @@ bool tlshd_config_init(const gchar *pathname)
 					  "nl", NULL);
 	tmp = g_key_file_get_integer(tlshd_configuration, "debug",
 				     "delay_done", NULL);
-	tlshd_delay_done = tmp > 0 ? tmp : 0;
+	tlshd_delay_done = tmp > 0 ? (unsigned int)tmp : 0;
 
 	keyrings = g_key_file_get_string_list(tlshd_configuration,
 					      "authenticate",