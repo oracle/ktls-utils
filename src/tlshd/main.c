oracle/ktls-utils

commit 175030fa5edd5b325cb7666b8691ac60365cf005

fix handling of command like options
- help doesn't require argument
- exit after usage is printed

Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

src/tlshd/main.c
@@ -2,7 +2,7 @@
  * Handle a request for a TLS handshake on behalf of an
  * in-kernel TLS consumer.
  *
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022 - 2023 Oracle and/or its affiliates.
  *
  * ktls-utils is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -47,7 +47,7 @@
 
 #include "tlshd.h"
 
-static const char *optstring = "c:h:sv";
+static const char *optstring = "c:hsv";
 static const struct option longopts[] = {
 	{ "config",	required_argument,	NULL,	'c' },
 	{ "help",	no_argument,		NULL,	'h' },
@@ -55,6 +55,10 @@ static const struct option longopts[] = {
 	{ "version",	no_argument,		NULL,	'v' },
 	{ NULL,		0,			NULL,	 0 }
 };
+
+void usage(char *progname) {
+		fprintf(stderr, "usage: %s [-chsv]\n", progname);
+}
 
 int main(int argc, char **argv)
 {
@@ -85,8 +89,11 @@ int main(int argc, char **argv)
 				progname);
 			return EXIT_SUCCESS;
 		case 'h':
+			usage(progname);
+			return EXIT_SUCCESS;
 		default:
-			fprintf(stderr, "usage: %s [-chsv]\n", progname);
+			usage(progname);
+			return EXIT_FAILURE;
 		}
 	}
 

commit 1cfcc20221bc420efcc5c087b3ef7ec816560d8c

tlshd: Externally visible function definition with no forward declaration
There are no call sites of "usage()" outside of main.c, so make
it static.

Reported-by: Parfait 9.0 (#2141)
Fixes: 175030fa5edd ("fix handling of command like options")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/main.c
@@ -56,8 +56,9 @@ static const struct option longopts[] = {
 	{ NULL,		0,			NULL,	 0 }
 };
 
-void usage(char *progname) {
-		fprintf(stderr, "usage: %s [-chsv]\n", progname);
+static void usage(char *progname)
+{
+	fprintf(stderr, "usage: %s [-chsv]\n", progname);
 }
 
 int main(int argc, char **argv)