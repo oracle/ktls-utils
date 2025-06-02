oracle/ktls-utils

commit c380357cf8f1b0693b32ce3d317f1ce73e4b6f1c

tlshd: Demote warnings about missing options
The truststore, certificate, and private key options are not
mandatory, so it is not a full-fledged error if tlshd cannot find
them in tlshd.conf. This commit reduces log noise when the debugging
level is zero.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/config.c
@@ -203,7 +203,6 @@ bool tlshd_config_get_client_truststore(char **bundle)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
 					 "x509.truststore", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Client x.509 truststore not specified", error);
 		g_error_free(error);
 		return false;
 	}
@@ -235,7 +234,6 @@ bool tlshd_config_get_client_cert(gnutls_pcert_st *cert)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
 					"x509.certificate", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Default certificate not found", error);
 		g_error_free(error);
 		return false;
 	}
@@ -279,7 +277,6 @@ bool tlshd_config_get_client_privkey(gnutls_privkey_t *privkey)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
 					"x509.private_key", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Default private key not found", error);
 		g_error_free(error);
 		return false;
 	}
@@ -329,7 +326,6 @@ bool tlshd_config_get_server_truststore(char **bundle)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
 					 "x509.truststore", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Server x.509 truststore not specified", error);
 		g_error_free(error);
 		return false;
 	}
@@ -361,7 +357,6 @@ bool tlshd_config_get_server_cert(gnutls_pcert_st *cert)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
 					"x509.certificate", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Default certificate not found", error);
 		g_error_free(error);
 		return false;
 	}
@@ -404,7 +399,6 @@ bool tlshd_config_get_server_privkey(gnutls_privkey_t *privkey)
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
 					"x509.private_key", &error);
 	if (!pathname) {
-		tlshd_log_gerror("Default private key not found", error);
 		g_error_free(error);
 		return false;
 	}

commit dbdedccfd22e804623518eb82f7bc0f249a0b5ac

tlshd: Remove unneeded variable "error"
Clean up: Commit c380357cf8f1 ("tlshd: Demote warnings about missing
options") removed some debug messages that used the local "error"
variable. "error" is passed to glib, but the returned value is no
longer checked or used.

Fixes: c380357cf8f1 ("tlshd: Demote warnings about missing options")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/config.c
@@ -193,15 +193,13 @@ out:
  */
 bool tlshd_config_get_client_truststore(char **bundle)
 {
-	GError *error = NULL;
 	gchar *pathname;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
-					 "x509.truststore", &error);
-	if (!pathname) {
-		g_error_free(error);
+					 "x509.truststore", NULL);
+	if (!pathname)
 		return false;
-	} else if (access(pathname, F_OK)) {
+	if (access(pathname, F_OK)) {
 		tlshd_log_debug("tlshd cannot access \"%s\"", pathname);
 		g_free(pathname);
 		return false;
@@ -228,17 +226,14 @@ bool tlshd_config_get_client_truststore(char **bundle)
 bool tlshd_config_get_client_certs(gnutls_pcert_st *certs,
 				   unsigned int *certs_len)
 {
-	GError *error = NULL;
 	gnutls_datum_t data;
 	gchar *pathname;
 	int ret;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
-					"x509.certificate", &error);
-	if (!pathname) {
-		g_error_free(error);
+					"x509.certificate", NULL);
+	if (!pathname)
 		return false;
-	}
 
 	if (!tlshd_config_read_datum(pathname, &data, TLSHD_OWNER,
 				     TLSHD_CERT_MODE)) {
@@ -272,17 +267,14 @@ bool tlshd_config_get_client_certs(gnutls_pcert_st *certs,
  */
 bool tlshd_config_get_client_privkey(gnutls_privkey_t *privkey)
 {
-	GError *error = NULL;
 	gnutls_datum_t data;
 	gchar *pathname;
 	int ret;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.client",
-					"x509.private_key", &error);
-	if (!pathname) {
-		g_error_free(error);
+					"x509.private_key", NULL);
+	if (!pathname)
 		return false;
-	}
 
 	if (!tlshd_config_read_datum(pathname, &data, TLSHD_OWNER,
 				     TLSHD_PRIVKEY_MODE)) {
@@ -323,15 +315,13 @@ bool tlshd_config_get_client_privkey(gnutls_privkey_t *privkey)
  */
 bool tlshd_config_get_server_truststore(char **bundle)
 {
-	GError *error = NULL;
 	gchar *pathname;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
-					 "x509.truststore", &error);
-	if (!pathname) {
-		g_error_free(error);
+					 "x509.truststore", NULL);
+	if (!pathname)
 		return false;
-	} else if (access(pathname, F_OK)) {
+	if (access(pathname, F_OK)) {
 		tlshd_log_debug("tlshd cannot access \"%s\"", pathname);
 		g_free(pathname);
 		return false;
@@ -358,17 +348,14 @@ bool tlshd_config_get_server_truststore(char **bundle)
 bool tlshd_config_get_server_certs(gnutls_pcert_st *certs,
 				   unsigned int *certs_len)
 {
-	GError *error = NULL;
 	gnutls_datum_t data;
 	gchar *pathname;
 	int ret;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
-					"x509.certificate", &error);
-	if (!pathname) {
-		g_error_free(error);
+					"x509.certificate", NULL);
+	if (!pathname)
 		return false;
-	}
 
 	if (!tlshd_config_read_datum(pathname, &data, TLSHD_OWNER,
 				     TLSHD_CERT_MODE)) {
@@ -402,17 +389,14 @@ bool tlshd_config_get_server_certs(gnutls_pcert_st *certs,
  */
 bool tlshd_config_get_server_privkey(gnutls_privkey_t *privkey)
 {
-	GError *error = NULL;
 	gnutls_datum_t data;
 	gchar *pathname;
 	int ret;
 
 	pathname = g_key_file_get_string(tlshd_configuration, "authenticate.server",
-					"x509.private_key", &error);
-	if (!pathname) {
-		g_error_free(error);
+					"x509.private_key", NULL);
+	if (!pathname)
 		return false;
-	}
 
 	if (!tlshd_config_read_datum(pathname, &data, TLSHD_OWNER,
 				     TLSHD_PRIVKEY_MODE)) {