oracle/ktls-utils

commit 731d1b2bf8d6427226becf3b4c237ea65f41134c

tlshd: Handshake needs to check for CERTIFICATE_ERROR
tlshd's certificate verification functions return
GNUTLS_E_CERTIFICATE_ERROR, not
GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR, when certification
verification is unsuccessful.

This gets rid of an unexpected journal message:

  tlshd_start_tls_handshake unhandled error -43, returning EACCES

Fixes: ecaf6608da74 ("tlshd: Report peer certificate verification failures")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/handshake.c
@@ -93,7 +93,7 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 		/* Any errors here should default to blocking access: */
 		parms->session_status = EACCES;
 		switch (ret) {
-		case GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR:
+		case GNUTLS_E_CERTIFICATE_ERROR:
 			tlshd_log_cert_verification_error(session);
 			break;
 		case -ETIMEDOUT:

commit 5c5b73076db06fe873c2f8f6d4fc2cb979fe3338

tlshd: Restore GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR
Client handshakes return CERTIFICATE_VERIFICATION_ERROR.

Fixes: 731d1b2bf8d6 ("tlshd: Handshake needs to check for CERTIFICATE_ERROR")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/handshake.c
@@ -94,6 +94,7 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 		parms->session_status = EACCES;
 		switch (ret) {
 		case GNUTLS_E_CERTIFICATE_ERROR:
+		case GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR:
 			tlshd_log_cert_verification_error(session);
 			break;
 		case -ETIMEDOUT: