oracle/ktls-utils

commit 7655d96c7ace36618e32eda289271ddb4b9aaa80

tlshd: Move peername/peeraddr preparation
Refactor / clean up: move the peername and peeraddr completely into
the handshake parameters structure.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/netlink.c
@@ -218,10 +218,14 @@ static void tlshd_parse_certificate(struct tlshd_handshake_parms *parms,
 		parms->x509_privkey = nla_get_u32(tb[HANDSHAKE_A_X509_PRIVKEY]);
 }
 
+static char tlshd_peername[NI_MAXHOST] = "unknown";
+static struct sockaddr_storage tlshd_peeraddr;
+
 static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 {
 	struct nlattr *tb[HANDSHAKE_A_ACCEPT_MAX + 1];
 	struct tlshd_handshake_parms *parms = arg;
+	char *peername = NULL;
 	int err;
 
 	tlshd_log_debug("Parsing a valid netlink message\n");
@@ -233,12 +237,18 @@ static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 		return NL_STOP;
 	}
 
-	if (tb[HANDSHAKE_A_ACCEPT_SOCKFD])
+	if (tb[HANDSHAKE_A_ACCEPT_SOCKFD]) {
 		parms->sockfd = nla_get_u32(tb[HANDSHAKE_A_ACCEPT_SOCKFD]);
+		if (getpeername(parms->sockfd, parms->peeraddr,
+				&parms->peeraddr_len) == -1) {
+			tlshd_log_perror("getpeername");
+			return NL_STOP;
+		}
+	}
 	if (tb[HANDSHAKE_A_ACCEPT_MESSAGE_TYPE])
 		parms->handshake_type = nla_get_u32(tb[HANDSHAKE_A_ACCEPT_MESSAGE_TYPE]);
 	if (tb[HANDSHAKE_A_ACCEPT_PEERNAME])
-		parms->peername = nla_get_string(tb[HANDSHAKE_A_ACCEPT_PEERNAME]);
+		peername = nla_get_string(tb[HANDSHAKE_A_ACCEPT_PEERNAME]);
 	if (tb[HANDSHAKE_A_ACCEPT_TIMEOUT])
 		parms->timeout_ms = nla_get_u32(tb[HANDSHAKE_A_ACCEPT_TIMEOUT]);
 	if (tb[HANDSHAKE_A_ACCEPT_AUTH_MODE])
@@ -247,11 +257,25 @@ static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 	tlshd_parse_peer_identity(parms, tb[HANDSHAKE_A_ACCEPT_PEER_IDENTITY]);
 	tlshd_parse_certificate(parms, tb[HANDSHAKE_A_ACCEPT_CERTIFICATE]);
 
+	if (peername)
+		strcpy(tlshd_peername, peername);
+	else {
+		err = getnameinfo(parms->peeraddr, parms->peeraddr_len,
+				  tlshd_peername, sizeof(tlshd_peername),
+				  NULL, 0, NI_NAMEREQD);
+		if (err) {
+			tlshd_log_gai_error(err);
+			return NL_STOP;
+		}
+	}
+
 	return NL_SKIP;
 }
 
 static const struct tlshd_handshake_parms tlshd_default_handshake_parms = {
-	.peername		= NULL,
+	.peername		= tlshd_peername,
+	.peeraddr		= (struct sockaddr *)&tlshd_peeraddr,
+	.peeraddr_len		= sizeof(tlshd_peeraddr),
 	.sockfd			= -1,
 	.handshake_type		= HANDSHAKE_MSG_TYPE_UNSPEC,
 	.timeout_ms		= GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT,

src/tlshd/handshake.c
@@ -120,32 +120,10 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
  */
 void tlshd_service_socket(void)
 {
-	static char peername[NI_MAXHOST] = "unknown";
 	struct tlshd_handshake_parms parms;
-	static struct sockaddr_storage ss;
-	static socklen_t peeraddr_len;
-	struct sockaddr *peeraddr = (struct sockaddr *)&ss;
-	int ret;
 
-	memset(&ss, 0, sizeof(ss));
-	peeraddr_len = 0;
 	if (tlshd_genl_get_handshake_parms(&parms) != 0)
 		goto out;
-	peeraddr_len = sizeof(ss);
-	if (getpeername(parms.sockfd, peeraddr, &peeraddr_len) == -1) {
-		tlshd_log_perror("getpeername");
-		goto out;
-	}
-	if (!parms.peername) {
-		ret = getnameinfo(peeraddr, peeraddr_len, peername,
-				  sizeof(peername), NULL, 0, NI_NAMEREQD);
-		if (ret) {
-			tlshd_log_gai_error(ret);
-			goto out;
-		}
-	} else
-		strcpy(peername, parms.peername);
-	parms.peername = peername;
 
 	switch (parms.handshake_type) {
 	case HANDSHAKE_MSG_TYPE_CLIENTHELLO:
@@ -165,8 +143,9 @@ out:
 	free(parms.peerids);
 
 	if (parms.session_status) {
-		tlshd_log_failure(peername, peeraddr, peeraddr_len);
+		tlshd_log_failure(parms.peername, parms.peeraddr,
+				  parms.peeraddr_len);
 		return;
 	}
-	tlshd_log_success(peername, peeraddr, peeraddr_len);
+	tlshd_log_success(parms.peername, parms.peeraddr, parms.peeraddr_len);
 }

src/tlshd/tlshd.h
@@ -28,6 +28,8 @@ struct nl_sock;
 
 struct tlshd_handshake_parms {
 	char		*peername;
+	struct sockaddr *peeraddr;
+	socklen_t	peeraddr_len;
 	int		sockfd;
 	int		handshake_type;
 	unsigned int	timeout_ms;

commit 808a57f8b9c01b393dbc34cd129c46a09edd288a

tlshd: Dynamically allocate hostname
Ensure that, when allocation or DNS query fails, parms->peername
always remains NULL. tlshd should not assume that the buffer
returned from getnameinfo(3) will remain untouched if that call
is unsuccessful.

Fixes: 7655d96c7ace ("tlshd: Move peername/peeraddr preparation")
Reported-by: Ken Milmore <ken.milmore@gmail.com>
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/log.c
@@ -61,7 +61,7 @@ void tlshd_log_completion(struct tlshd_handshake_parms *parms)
 		status = "failed";
 		priority = LOG_ERR;
 	}
-	if (parms->peeraddr)
+	if (parms->peername && parms->peeraddr)
 		syslog(priority, "Handshake with '%s' (%s) %s\n",
 		       parms->peername, parms->peeraddr, status);
 	else

src/tlshd/netlink.c
@@ -232,8 +232,6 @@ static void tlshd_parse_certificate(struct tlshd_handshake_parms *parms,
 		parms->x509_privkey = nla_get_s32(tb[HANDSHAKE_A_X509_PRIVKEY]);
 }
 
-static char tlshd_peername[NI_MAXHOST] = "unknown";
-
 static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 {
 	struct nlattr *tb[HANDSHAKE_A_ACCEPT_MAX + 1];
@@ -304,22 +302,24 @@ static int tlshd_genl_valid_handler(struct nl_msg *msg, void *arg)
 	tlshd_parse_certificate(parms, tb[HANDSHAKE_A_ACCEPT_CERTIFICATE]);
 
 	if (peername)
-		strncpy(tlshd_peername, peername, sizeof(tlshd_peername) - 1);
+		parms->peername = strdup(peername);
 	else if (sap) {
-		err = getnameinfo(sap, salen,
-				  tlshd_peername, sizeof(tlshd_peername),
+		char buf[NI_MAXHOST];
+
+		err = getnameinfo(sap, salen, buf, sizeof(buf),
 				  NULL, 0, NI_NAMEREQD);
 		if (err) {
 			tlshd_log_gai_error(err);
 			return NL_STOP;
 		}
+		parms->peername = strdup(buf);
 	}
 
 	return NL_SKIP;
 }
 
 static const struct tlshd_handshake_parms tlshd_default_handshake_parms = {
-	.peername		= tlshd_peername,
+	.peername		= NULL,
 	.peeraddr		= NULL,
 	.sockfd			= -1,
 	.ip_proto		= -1,
@@ -426,6 +426,7 @@ void tlshd_genl_put_handshake_parms(struct tlshd_handshake_parms *parms)
 		keyctl_unlink(parms->keyring, KEY_SPEC_SESSION_KEYRING);
 	g_array_free(parms->peerids, TRUE);
 	g_array_free(parms->remote_peerids, TRUE);
+	free(parms->peername);
 	free(parms->peeraddr);
 }
 