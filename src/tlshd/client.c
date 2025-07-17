oracle/ktls-utils

commit fe3a78aaef92f5b7ab537840d104808d87f03d0a

tlshd: Refactor trust store management
Prepare for implementing support for certificate revocation lists
by de-duplicating the code that reads in the trust store.

Suggested-by: Long Xin <lxin@redhat.com>
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/client.c
@@ -43,12 +43,31 @@
 #include "tlshd.h"
 #include "netlink.h"
 
+static int tlshd_client_get_truststore(gnutls_certificate_credentials_t cred)
+{
+	char *pathname;
+	int ret;
+
+	if (tlshd_config_get_client_truststore(&pathname)) {
+		ret = gnutls_certificate_set_x509_trust_file(cred, pathname,
+							     GNUTLS_X509_FMT_PEM);
+		free(pathname);
+	} else
+		ret = gnutls_certificate_set_x509_system_trust(cred);
+	if (ret < 0) {
+		tlshd_log_gnutls_error(ret);
+		return ret;
+	}
+	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
+
+	return GNUTLS_E_SUCCESS;
+}
+
 static void tlshd_tls13_client_anon_handshake(struct tlshd_handshake_parms *parms)
 {
 	gnutls_certificate_credentials_t xcred;
 	gnutls_session_t session;
 	unsigned int flags;
-	char *cafile;
 	int ret;
 
 	ret = gnutls_certificate_allocate_credentials(&xcred);
@@ -65,17 +84,9 @@ static void tlshd_tls13_client_anon_handshake(struct tlshd_handshake_parms *parm
 	gnutls_certificate_set_flags(xcred,
 			GNUTLS_CERTIFICATE_SKIP_KEY_CERT_MATCH | GNUTLS_CERTIFICATE_SKIP_OCSP_RESPONSE_CHECK);
 
-	if (tlshd_config_get_client_truststore(&cafile)) {
-		ret = gnutls_certificate_set_x509_trust_file(xcred, cafile,
-							     GNUTLS_X509_FMT_PEM);
-		free(cafile);
-	} else
-		ret = gnutls_certificate_set_x509_system_trust(xcred);
-	if (ret < 0) {
-		tlshd_log_gnutls_error(ret);
+	ret = tlshd_client_get_truststore(xcred);
+	if (ret != GNUTLS_E_SUCCESS)
 		goto out_free_creds;
-	}
-	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
 
 	flags = GNUTLS_CLIENT;
 	ret = gnutls_init(&session, flags);
@@ -244,7 +255,6 @@ static void tlshd_tls13_client_x509_handshake(struct tlshd_handshake_parms *parm
 	gnutls_certificate_credentials_t xcred;
 	gnutls_session_t session;
 	unsigned int flags;
-	char *cafile;
 	int ret;
 
 	ret = gnutls_certificate_allocate_credentials(&xcred);
@@ -253,17 +263,9 @@ static void tlshd_tls13_client_x509_handshake(struct tlshd_handshake_parms *parm
 		return;
 	}
 
-	if (tlshd_config_get_client_truststore(&cafile)) {
-		ret = gnutls_certificate_set_x509_trust_file(xcred, cafile,
-							     GNUTLS_X509_FMT_PEM);
-		free(cafile);
-	} else
-		ret = gnutls_certificate_set_x509_system_trust(xcred);
-	if (ret < 0) {
-		tlshd_log_gnutls_error(ret);
+	ret = tlshd_client_get_truststore(xcred);
+	if (ret != GNUTLS_E_SUCCESS)
 		goto out_free_creds;
-	}
-	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
 
 	if (!tlshd_x509_client_get_certs(parms))
 		goto out_free_creds;
@@ -463,7 +465,6 @@ static int tlshd_quic_client_set_x509_session(struct tlshd_quic_conn *conn)
 	gnutls_certificate_credentials_t cred;
 	gnutls_session_t session;
 	int ret = -EINVAL;
-	char *cafile;
 
 	if (conn->cert_req != TLSHD_QUIC_NO_CERT_AUTH) {
 		if (!tlshd_x509_client_get_certs(parms) || !tlshd_x509_client_get_privkey(parms)) {
@@ -474,14 +475,9 @@ static int tlshd_quic_client_set_x509_session(struct tlshd_quic_conn *conn)
 	ret = gnutls_certificate_allocate_credentials(&cred);
 	if (ret)
 		goto err;
-	if (tlshd_config_get_client_truststore(&cafile)) {
-		ret = gnutls_certificate_set_x509_trust_file(cred, cafile, GNUTLS_X509_FMT_PEM);
-		free(cafile);
-	} else
-		ret = gnutls_certificate_set_x509_system_trust(cred);
-	if (ret < 0)
+	ret = tlshd_client_get_truststore(xcred);
+	if (ret != GNUTLS_E_SUCCESS)
 		goto err_cred;
-	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
 
 	if (conn->cert_req == TLSHD_QUIC_NO_CERT_AUTH) {
 		gnutls_certificate_set_verify_flags(cred, GNUTLS_VERIFY_ALLOW_SIGN_RSA_MD2 |

src/tlshd/server.c
@@ -133,6 +133,24 @@ tlshd_x509_retrieve_key_cb(gnutls_session_t session,
 	return 0;
 }
 
+static int tlshd_server_get_truststore(gnutls_certificate_credentials_t cred)
+{
+	char *pathname;
+	int ret;
+
+	if (tlshd_config_get_server_truststore(&pathname)) {
+		ret = gnutls_certificate_set_x509_trust_file(cred, pathname,
+							     GNUTLS_X509_FMT_PEM);
+		free(pathname);
+	} else
+		ret = gnutls_certificate_set_x509_system_trust(cred);
+	if (ret < 0)
+		return ret;
+	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
+
+	return GNUTLS_E_SUCCESS;
+}
+
 /**
  * tlshd_server_x509_verify_function - Verify remote's x.509 certificate
  * @session: session in the midst of a handshake
@@ -190,7 +208,6 @@ static void tlshd_tls13_server_x509_handshake(struct tlshd_handshake_parms *parm
 {
 	gnutls_certificate_credentials_t xcred;
 	gnutls_session_t session;
-	char *cafile;
 	int ret;
 
 	ret = gnutls_certificate_allocate_credentials(&xcred);
@@ -198,18 +215,9 @@ static void tlshd_tls13_server_x509_handshake(struct tlshd_handshake_parms *parm
 		tlshd_log_gnutls_error(ret);
 		return;
 	}
-
-	if (tlshd_config_get_server_truststore(&cafile)) {
-		ret = gnutls_certificate_set_x509_trust_file(xcred, cafile,
-							     GNUTLS_X509_FMT_PEM);
-		free(cafile);
-	} else
-		ret = gnutls_certificate_set_x509_system_trust(xcred);
-	if (ret < 0) {
-		tlshd_log_gnutls_error(ret);
+	ret = tlshd_server_get_truststore(xcred);
+	if (ret != GNUTLS_E_SUCCESS)
 		goto out_free_creds;
-	}
-	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
 
 	if (!tlshd_x509_server_get_certs(parms))
 		goto out_free_creds;
@@ -471,15 +479,9 @@ static int tlshd_quic_server_set_x509_session(struct tlshd_quic_conn *conn)
 	ret = gnutls_certificate_allocate_credentials(&cred);
 	if (ret)
 		goto err;
-	if (tlshd_config_get_server_truststore(&cafile)) {
-		ret = gnutls_certificate_set_x509_trust_file(cred, cafile,
-							     GNUTLS_X509_FMT_PEM);
-		free(cafile);
-	} else
-		ret = gnutls_certificate_set_x509_system_trust(cred);
-	if (ret < 0)
-		goto err_cred;
-	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
+	ret = tlshd_server_get_truststore(cred);
+	if (ret)
+		goto err;
 
 	gnutls_certificate_set_retrieve_function2(cred, tlshd_x509_retrieve_key_cb);
 

commit 8fea967ff1e6635ceb27850aedbf7d04fadafe62

tlshd: fix a couple of compiling errors with QUIC enabled
This patch fixes these compiling errors with QUIC enabled:

client.c: In function ‘tlshd_quic_client_set_x509_session’:
client.c:489:43: error: ‘xcred’ undeclared (first use in this function);
  did you mean ‘cred’?
  489 |         ret = tlshd_client_get_truststore(xcred);
      |                                           ^~~~~
      |                                           cred

client.c:489:43: note: each undeclared identifier is reported only once
  for each function it appears in
client.c: In function ‘tlshd_quic_client_set_psk_session’:
client.c:544:31: error: incompatible types when initializing type
  ‘key_serial_t’ {aka ‘int’} using type ‘GArray’ {aka ‘struct _GArray’}
  544 |         key_serial_t peerid = conn->parms->peerids[0];
      |                               ^~~~

server.c: In function ‘tlshd_quic_server_set_x509_session’:
server.c:481:15: error: unused variable ‘cafile’ [-Werror=unused-variable]
  481 |         char *cafile;
      |               ^~~~~~

Commit fe3a78aaef92 ("tlshd: Refactor trust store management") passed
incorrect 'xcred' to tlshd_client_get_truststore(), while it should
be 'cred'. The unused variable 'cafile' should also be cleaned in
tlshd_quic_server_set_x509_session().

Commit 18b04a63fc01 ("tlshd: Store peer IDs in a GArray") forgot to
fix the parms->peerids access in tlshd_quic_client_set_psk_session().

Fixes: fe3a78aaef92 ("tlshd: Refactor trust store management")
Fixes: 18b04a63fc01 ("tlshd: Store peer IDs in a GArray")
Signed-off-by: Xin Long <lucien.xin@gmail.com>

src/tlshd/client.c
@@ -486,7 +486,7 @@ static int tlshd_quic_client_set_x509_session(struct tlshd_quic_conn *conn)
 	ret = gnutls_certificate_allocate_credentials(&cred);
 	if (ret)
 		goto err;
-	ret = tlshd_client_get_truststore(xcred);
+	ret = tlshd_client_get_truststore(cred);
 	if (ret != GNUTLS_E_SUCCESS)
 		goto err_cred;
 
@@ -541,7 +541,7 @@ static int tlshd_quic_client_set_anon_session(struct tlshd_quic_conn *conn)
 
 static int tlshd_quic_client_set_psk_session(struct tlshd_quic_conn *conn)
 {
-	key_serial_t peerid = conn->parms->peerids[0];
+	key_serial_t peerid = g_array_index(conn->parms->peerids, key_serial_t, 0);
 	gnutls_psk_client_credentials_t cred;
 	gnutls_session_t session;
 	char *identity = NULL;

src/tlshd/server.c
@@ -478,7 +478,6 @@ static int tlshd_quic_server_set_x509_session(struct tlshd_quic_conn *conn)
 	gnutls_datum_t ticket_key;
 	gnutls_session_t session;
 	int ret = -EINVAL;
-	char *cafile;
 
 	if (!tlshd_x509_server_get_certs(parms) || !tlshd_x509_server_get_privkey(parms)) {
 		tlshd_log_error("cert/privkey get error %d", -ret);