d618c1414a6a06c1b972ce39a62cc4572c323d1e
Chuck Lever <chuck.lever@oracle.com>
2025-08-04 15:58:31 -0400

    tlshd: Relocate /etc/tlshd.conf
    To support TLS session tags, additional configuration files
    outside of /etc/tlshd.conf will be needed. So create a tlshd
    configuration directory:
    
       /etc/tlshd/
    
    And move /etc/tlshd.conf into that directory as:
    
       /etc/tlshd/config
    
    For now, tlshd will look for /etc/tlshd.conf first, and warn
    that the new file name should be used instead.
    
    The man page tlshd.conf.5 is retained. Another option is to call
    it tlshd_config.5.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- src/tlshd/tlshd.h
+++ src/tlshd/tlshd.h
@@ -50,7 +50,7 @@ extern void tlshd_tls13_clienthello_handshake(struct tlshd_handshake_parms *parm
 extern void tlshd_quic_clienthello_handshake(struct tlshd_handshake_parms *parms);
 
 /* config.c */
-bool tlshd_config_init(const gchar *pathname);
+bool tlshd_config_init(const gchar *pathname, bool legacy);
 void tlshd_config_shutdown(void);
 bool tlshd_config_get_client_truststore(char **bundle);
 bool tlshd_config_get_client_crl(char **result);
