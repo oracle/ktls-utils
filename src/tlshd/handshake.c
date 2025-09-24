oracle/ktls-utils

commit b010190cfed2d9bbd943b3343fdbaeb66efff8e8

tlshd: Pass ETIMEDOUT from gnutls to kernel
We've had some QE work that's created a condition (some types of connection
instability) where the handshake attempt has timed out.  When this happens,
tlshd sends EACESS back to the kernel.  However, the kernel may not be
expecting this error in the context of some NFS operations, for example:
writeback.  It can handle ETIMEDOUT, and we would like the kernel to
perform its normal hard/soft retry routines for this case to re-connect to
the server.

Add an error switch that clearly denotes the error paths we'd like
to send back to the kernel.  For SUNRPC, there are other insteresting
errors that might be included (see call_conenct_status() in
net/sunrpc/clnt.c), but are ommitted here because we don't have evidence of
them in the wild

Signed-off-by: Benjamin Coddington <bcodding@redhat.com>

src/tlshd/handshake.c
@@ -94,10 +94,14 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 		case GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR:
 			tlshd_log_cert_verification_error(session);
 			break;
-		default:
+		case -ETIMEDOUT:
 			tlshd_log_gnutls_error(ret);
+			parms->session_status = -ret;
+			break;
+		default:
+			tlshd_log_notice("tlshd_start_tls_handshake unhandled error %d, returning EACCES\n", ret);
+			parms->session_status = EACCES;
 		}
-		parms->session_status = EACCES;
 		return;
 	}
 

commit b52ca7c0a9c167340950a10d8dc56be1b95f5c40

tlshd: Clean up logic in tlshd_start_tls_handshake()
gnutls_handshake() is supposed to return only a GNUTLS_E value, and
the session_status field is supposed to contain only a positive
errno.

GNUTLS_E_PREMATURE_TERMINATION is -110. It turns out that on x86,
-ETIMEDOUT is also -110. Make sure the correct symbolic constants
and auditing functions are utilized.

Fixes: b010190cfed2 ("tlshd: Pass ETIMEDOUT from gnutls to kernel")
Reviewed-by: Benjamin Coddington <bcodding@redhat.com>
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/handshake.c
@@ -97,12 +97,12 @@ void tlshd_start_tls_handshake(gnutls_session_t session,
 		case GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR:
 			tlshd_log_cert_verification_error(session);
 			break;
-		case -ETIMEDOUT:
-			tlshd_log_gnutls_error(ret);
-			parms->session_status = -ret;
+		case GNUTLS_E_PREMATURE_TERMINATION:
+			tlshd_log_error("Handshake timeout, retrying");
+			parms->session_status = ETIMEDOUT;
 			break;
 		default:
-			tlshd_log_notice("tlshd_start_tls_handshake unhandled error %d, returning EACCES\n", ret);
+			tlshd_log_gnutls_error(ret);
 		}
 		return;
 	}