oracle/ktls-utils

commit 43a15fed2f3328f17b52507ce7c4bc034165abf7

tlshd: add support for quic handshake
This patch adds QUIC handshake support to tlshd, utilizing GnuTLS for
TLS message handling and integrating with existing code for PSK and
certificate verification. The feature is conditionally compiled
based on kernel and GnuTLS capabilities.

- QUIC handshake implementation:

In quic.c, these handshake APIs utilizes sendmsg() and recvmsg() with
the control message QUIC_HANDSHAKE_INFO to send and receive raw TLS
messages between kernel and userspace on QUIC type sockets and then
process them via GnuTLS.

Socket options are used to extract necessary Transport Parameters from
kernel, which are essential for constructing TLS messages. The Derived
secrets for different crypto levels are set to kernel to facilitate
QUIC packet encryption and decryption.

- Integration into tlshd:

In tlshd_service_socket() within handshake.c, if the socket's ip_proto
is IPPROTO_QUIC, the QUIC handshake functions are invoked to initiate
the QUIC handshake.

In configure.ac, the QUIC handshake support is conditionally enabled.
It requires the presence of the QUIC kernel header file and the
gnutls_handshake_set_secret_function in GnuTLS (version 3.7.0 or later).

Signed-off-by: Xin Long <lucien.xin@gmail.com>

src/tlshd/client.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2022 Oracle and/or its affiliates.
  * Copyright (c) 2022 SUSE LLC.
+ * Copyright (c) 2024 Red Hat, Inc.
  *
  * ktls-utils is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -196,21 +197,20 @@ tlshd_x509_retrieve_key_cb(gnutls_session_t session,
 /**
  * tlshd_client_x509_verify_function - Verify remote's x.509 certificate
  * @session: session in the midst of a handshake
+ * @parms: handshake parameters
  *
  * Return values:
  *   %GNUTLS_E_SUCCESS: Incoming certificate has been successfully verified
  *   %GNUTLS_E_CERTIFICATE_ERROR: certificate verification failed
  */
-static int tlshd_client_x509_verify_function(gnutls_session_t session)
+static int tlshd_client_x509_verify_function(gnutls_session_t session,
+					     struct tlshd_handshake_parms *parms)
 {
-	struct tlshd_handshake_parms *parms;
 	const gnutls_datum_t *peercerts;
 	gnutls_certificate_type_t type;
 	unsigned int i, status;
 	gnutls_datum_t out;
 	int ret;
-
-	parms = gnutls_session_get_ptr(session);
 
 	ret = gnutls_certificate_verify_peers3(session, parms->peername,
 					       &status);
@@ -260,6 +260,13 @@ static int tlshd_client_x509_verify_function(gnutls_session_t session)
 	}
 
 	return GNUTLS_E_SUCCESS;
+}
+
+static int tlshd_tls13_client_x509_verify_function(gnutls_session_t session)
+{
+	struct tlshd_handshake_parms *parms = gnutls_session_get_ptr(session);
+
+	return tlshd_client_x509_verify_function(session, parms);
 }
 
 static void tlshd_tls13_client_x509_handshake(struct tlshd_handshake_parms *parms)
@@ -321,7 +328,7 @@ static void tlshd_tls13_client_x509_handshake(struct tlshd_handshake_parms *parm
 		goto out_free_creds;
 	}
 	gnutls_certificate_set_verify_function(xcred,
-					       tlshd_client_x509_verify_function);
+					       tlshd_tls13_client_x509_verify_function);
 
 	tlshd_start_tls_handshake(session, parms);
 
@@ -439,3 +446,213 @@ void tlshd_tls13_clienthello_handshake(struct tlshd_handshake_parms *parms)
 				parms->auth_mode);
 	}
 }
+
+#ifdef HAVE_GNUTLS_QUIC
+static int tlshd_quic_client_x509_verify_function(gnutls_session_t session)
+{
+	struct tlshd_quic_conn *conn = gnutls_session_get_ptr(session);
+
+	return tlshd_client_x509_verify_function(session, conn->parms);
+}
+
+static int tlshd_quic_client_ticket_recv(gnutls_session_t session, unsigned int htype,
+					 unsigned int when, unsigned int incoming,
+					 const gnutls_datum_t *msg)
+{
+	struct tlshd_quic_conn *conn = gnutls_session_get_ptr(session);
+	int ret, sockfd = conn->parms->sockfd;
+	gnutls_datum_t ticket;
+
+	if (htype != GNUTLS_HANDSHAKE_NEW_SESSION_TICKET)
+		return 0;
+
+	conn->completed = 1;
+	ret = gnutls_session_get_data2(session, &ticket);
+	if (ret) {
+		tlshd_log_gnutls_error(ret);
+		return ret;
+	}
+
+	ret = setsockopt(sockfd, SOL_QUIC, QUIC_SOCKOPT_SESSION_TICKET, ticket.data, ticket.size);
+	if (ret) {
+		tlshd_log_error("socket setsockopt session ticket error %d %u", errno, ticket.size);
+		return -1;
+	}
+	tlshd_log_debug("  Ticket recv: %u %u %u", when, incoming, msg->size);
+	return 0;
+}
+
+#define TLSHD_QUIC_NO_CERT_AUTH	3
+
+static int tlshd_quic_client_set_x509_session(struct tlshd_quic_conn *conn)
+{
+	struct tlshd_handshake_parms *parms = conn->parms;
+	gnutls_certificate_credentials_t cred;
+	gnutls_session_t session;
+	int ret = -EINVAL;
+	char *cafile;
+
+	if (conn->cert_req != TLSHD_QUIC_NO_CERT_AUTH) {
+		if (!tlshd_x509_client_get_certs(parms) || !tlshd_x509_client_get_privkey(parms)) {
+			tlshd_log_error("cert/privkey get error %d", -ret);
+			return ret;
+		}
+	}
+	ret = gnutls_certificate_allocate_credentials(&cred);
+	if (ret)
+		goto err;
+	if (tlshd_config_get_client_truststore(&cafile)) {
+		ret = gnutls_certificate_set_x509_trust_file(cred, cafile, GNUTLS_X509_FMT_PEM);
+		free(cafile);
+	} else
+		ret = gnutls_certificate_set_x509_system_trust(cred);
+	if (ret < 0)
+		goto err_cred;
+	tlshd_log_debug("System trust: Loaded %d certificate(s).", ret);
+
+	if (conn->cert_req == TLSHD_QUIC_NO_CERT_AUTH) {
+		gnutls_certificate_set_verify_flags(cred, GNUTLS_VERIFY_ALLOW_SIGN_RSA_MD2 |
+							  GNUTLS_VERIFY_ALLOW_SIGN_RSA_MD5);
+		gnutls_certificate_set_flags(cred, GNUTLS_CERTIFICATE_SKIP_KEY_CERT_MATCH |
+						   GNUTLS_CERTIFICATE_SKIP_OCSP_RESPONSE_CHECK);
+	} else {
+		gnutls_certificate_set_verify_function(cred,
+						       tlshd_quic_client_x509_verify_function);
+		gnutls_certificate_set_retrieve_function2(cred, tlshd_x509_retrieve_key_cb);
+	}
+
+	ret = gnutls_init(&session, GNUTLS_CLIENT |
+				    GNUTLS_ENABLE_EARLY_DATA | GNUTLS_NO_END_OF_EARLY_DATA);
+	if (ret)
+		goto err_cred;
+	gnutls_handshake_set_hook_function(session, GNUTLS_HANDSHAKE_ANY,
+					   GNUTLS_HOOK_POST, tlshd_quic_client_ticket_recv);
+	gnutls_session_set_ptr(session, conn);
+	ret = tlshd_quic_session_configure(session, conn->alpns, conn->cipher);
+	if (ret)
+		goto err_session;
+	if (conn->ticket_len) {
+		ret = gnutls_session_set_data(session, conn->ticket, conn->ticket_len);
+		if (ret)
+			goto err_session;
+	}
+	ret = gnutls_credentials_set(session, GNUTLS_CRD_CERTIFICATE, cred);
+	if (ret)
+		goto err_session;
+	if (parms->peername) {
+		ret = gnutls_server_name_set(session, GNUTLS_NAME_DNS,
+					     parms->peername, strlen(parms->peername));
+		if (ret)
+			goto err_session;
+	}
+	conn->session = session;
+	return 0;
+err_session:
+	gnutls_deinit(session);
+err_cred:
+	gnutls_certificate_free_credentials(cred);
+err:
+	tlshd_log_gnutls_error(ret);
+	return ret;
+}
+
+static int tlshd_quic_client_set_anon_session(struct tlshd_quic_conn *conn)
+{
+	conn->cert_req = TLSHD_QUIC_NO_CERT_AUTH;
+	return tlshd_quic_client_set_x509_session(conn);
+}
+
+static int tlshd_quic_client_set_psk_session(struct tlshd_quic_conn *conn)
+{
+	key_serial_t peerid = conn->parms->peerids[0];
+	gnutls_psk_client_credentials_t cred;
+	gnutls_session_t session;
+	char *identity = NULL;
+	gnutls_datum_t key;
+	int ret = -EINVAL;
+
+	if (!tlshd_keyring_get_psk_username(peerid, &identity) ||
+	    !tlshd_keyring_get_psk_key(peerid, &key)) {
+		free(identity);
+		tlshd_log_error("identity/key get error %d", -ret);
+		return ret;
+	}
+
+	ret = gnutls_psk_allocate_client_credentials(&cred);
+	if (ret)
+		goto err;
+	ret = gnutls_psk_set_client_credentials(cred, identity, &key, GNUTLS_PSK_KEY_RAW);
+	if (ret)
+		goto err_cred;
+
+	ret = gnutls_init(&session, GNUTLS_CLIENT);
+	if (ret)
+		goto err_cred;
+	gnutls_handshake_set_hook_function(session, GNUTLS_HANDSHAKE_ANY,
+					   GNUTLS_HOOK_POST, tlshd_quic_client_ticket_recv);
+	gnutls_session_set_ptr(session, conn);
+	ret = tlshd_quic_session_configure(session, conn->alpns, conn->cipher);
+	if (ret)
+		goto err_session;
+	ret = gnutls_credentials_set(session, GNUTLS_CRD_PSK, cred);
+	if (ret)
+		goto err_session;
+	conn->session = session;
+	return 0;
+err_session:
+	gnutls_deinit(session);
+err_cred:
+	gnutls_psk_free_client_credentials(cred);
+err:
+	free(identity);
+	tlshd_log_gnutls_error(ret);
+	return ret;
+}
+
+/**
+ * tlshd_quic_clienthello_handshake - send a QUIC Client Initial
+ * @parms: handshake parameters
+ *
+ */
+void tlshd_quic_clienthello_handshake(struct tlshd_handshake_parms *parms)
+{
+	struct tlshd_quic_conn *conn;
+	int ret;
+
+	ret = tlshd_quic_conn_create(&conn, parms);
+	if (ret) {
+		parms->session_status = -ret;
+		return gnutls_global_deinit();
+	}
+
+	switch (parms->auth_mode) {
+	case HANDSHAKE_AUTH_UNAUTH:
+		ret = tlshd_quic_client_set_anon_session(conn);
+		break;
+	case HANDSHAKE_AUTH_X509:
+		ret = tlshd_quic_client_set_x509_session(conn);
+		break;
+	case HANDSHAKE_AUTH_PSK:
+		ret = tlshd_quic_client_set_psk_session(conn);
+		break;
+	default:
+		ret = -EINVAL;
+		tlshd_log_debug("Unrecognized auth mode (%d)", parms->auth_mode);
+	}
+	if (ret) {
+		conn->errcode = -ret;
+		goto out;
+	}
+
+	tlshd_quic_start_handshake(conn);
+out:
+	parms->session_status = conn->errcode;
+	tlshd_quic_conn_destroy(conn);
+}
+#else
+void tlshd_quic_clienthello_handshake(struct tlshd_handshake_parms *parms)
+{
+	tlshd_log_debug("QUIC handshake is not enabled (%d)", parms->auth_mode);
+	parms->session_status = EOPNOTSUPP;
+}
+#endif

configure.ac
@@ -60,6 +60,10 @@ PKG_CHECK_MODULES([LIBNL3], libnl-3.0 >= 3.1)
 AC_SUBST([LIBNL3_CFLAGS])
 AC_SUBST([LIBNL3_LIBS])
 
+AC_CHECK_FILE([/usr/include/linux/quic.h],
+              [AC_CHECK_LIB([gnutls], [gnutls_handshake_set_secret_function],
+                            [AC_DEFINE([HAVE_GNUTLS_QUIC], [1], [Define to 1 if QUIC is found.])])])
+
 AC_CHECK_LIB([gnutls], [gnutls_transport_is_ktls_enabled],
              [AC_DEFINE([HAVE_GNUTLS_TRANSPORT_IS_KTLS_ENABLED], [1],
                         [Define to 1 if you have the gnutls_transport_is_ktls_enabled function.])])

src/tlshd/Makefile.am
@@ -26,7 +26,7 @@ sbin_PROGRAMS		= tlshd
 tlshd_CFLAGS		= -Werror -Wall -Wextra $(LIBGNUTLS_CFLAGS) \
 			  $(LIBKEYUTILS_CFLAGS) $(GLIB_CFLAGS) $(LIBNL3_CFLAGS)
 tlshd_SOURCES		= client.c config.c handshake.c keyring.c ktls.c log.c \
-			  main.c netlink.c netlink.h server.c tlshd.h
+			  main.c netlink.c netlink.h server.c tlshd.h quic.c
 tlshd_LDADD		= $(LIBGNUTLS_LIBS) $(LIBKEYUTILS_LIBS) $(GLIB_LIBS) \
 			  $(LIBNL3_LIBS) -lnl-genl-3
 

src/tlshd/handshake.c
@@ -138,10 +138,34 @@ void tlshd_service_socket(void)
 
 	switch (parms.handshake_type) {
 	case HANDSHAKE_MSG_TYPE_CLIENTHELLO:
-		tlshd_tls13_clienthello_handshake(&parms);
+		switch (parms.ip_proto) {
+		case IPPROTO_TCP:
+			tlshd_tls13_clienthello_handshake(&parms);
+			break;
+#ifdef HAVE_GNUTLS_QUIC
+		case IPPROTO_QUIC:
+			tlshd_quic_clienthello_handshake(&parms);
+			break;
+#endif
+		default:
+			tlshd_log_debug("Unsupported ip_proto (%d)", parms.ip_proto);
+			parms.session_status = EOPNOTSUPP;
+		}
 		break;
 	case HANDSHAKE_MSG_TYPE_SERVERHELLO:
-		tlshd_tls13_serverhello_handshake(&parms);
+		switch (parms.ip_proto) {
+		case IPPROTO_TCP:
+			tlshd_tls13_serverhello_handshake(&parms);
+			break;
+#ifdef HAVE_GNUTLS_QUIC
+		case IPPROTO_QUIC:
+			tlshd_quic_serverhello_handshake(&parms);
+			break;
+#endif
+		default:
+			tlshd_log_debug("Unsupported ip_proto (%d)", parms.ip_proto);
+			parms.session_status = EOPNOTSUPP;
+		}
 		break;
 	default:
 		tlshd_log_debug("Unrecognized handshake type (%d)",

commit 2ddd61bb65427182697416dfb76d600901987375

tlshd: Release key and certs on QUIC client x509 setup failure
When tlshd_x509_client_get_certs() or tlshd_x509_client_get_privkey()
fails while preparing a QUIC client x509 session, the early return
exits without releasing the module-global certificates and private
key that were already loaded, leaking them.  The TLS and QUIC server
paths route such failures through their cleanup labels, which call
the put_privkey() and put_certs() helpers; only the QUIC client path
took a bare return.

Route the failure through the cleanup path as the others do.  Split
the err: label so the configuration-failure path skips
tlshd_log_gnutls_error(), which would otherwise read an uninitialized
ret, and reaches only put_privkey() and put_certs() under a new
err_config: label.

Fixes: 43a15fed2f33 ("tlshd: add support for quic handshake")
Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

src/tlshd/client.c
@@ -649,7 +649,7 @@ static void tlshd_quic_client_set_x509_session(struct tlshd_quic_conn *conn)
 	if (conn->cert_req != TLSHD_QUIC_NO_CERT_AUTH) {
 		if (!tlshd_x509_client_get_certs(parms) || !tlshd_x509_client_get_privkey(parms)) {
 			tlshd_log_error("Failed to get cert or privkey");
-			return;
+			goto err_config;
 		}
 	}
 	ret = gnutls_certificate_allocate_credentials(&cred);
@@ -697,9 +697,10 @@ err_session:
 err_cred:
 	gnutls_certificate_free_credentials(cred);
 err:
+	tlshd_log_gnutls_error(ret);
+err_config:
 	tlshd_x509_client_put_privkey();
 	tlshd_x509_client_put_certs();
-	tlshd_log_gnutls_error(ret);
 }
 
 /**